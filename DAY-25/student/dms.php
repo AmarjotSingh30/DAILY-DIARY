60624b9b00e0ab4bdf528c6a3f5d9ebc076a0508
Amarjot SIngh <amarjt112@gmail.com>
2024-07-03 22:52:54 +0530

    DAILY DIARY

---
+++ DAY-25/student/dms.php
@@ -0,0 +1,50 @@
+<?php  
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title></title>
+	<style type="text/css">
+		*{
+			margin: 0;
+			padding: 0;
+			font-family: sans-serif;
+			box-sizing: border-box;	
+		}
+		table{
+			width: 50%;
+			height: auto;
+			border: 1px solid black;
+			margin: auto;
+/*			text-align: center;*/
+		}
+		tr >td,th{
+			height: 7vh;
+			border: 1px solid black;
+		}
+
+	</style>
+</head>
+<body>
+	<div>
+		<h1 style="text-align: center; text-decoration: underline;">Result</h1>
+		<table cellspacing="0" >
+			<tr>
+				<th>Name</th><td>Amarjot Singh</td>
+			</tr>
+			<tr>
+				<th>Father's Name</th><td>Amarjot</td>
+			</tr>
+			<tr>
+				<th>Mother's Name</th><td>Amarjot</td>
+			</tr>
+			<tr>
+				<th>Roll No.</th><td>1</td>
+			</tr>
+		</table>
+	</div>
+</body>
+</html>
